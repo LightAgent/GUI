014ea9dfe9087e7e480a220751a5a39106a5f523
LightAgent <65326874+LightAgent@users.noreply.github.com>
2023-02-01 08:23:33 +0200

    finalizing

--- Js/bindings.js
+++ Js/bindings.js
@@ -1,62 +1,57 @@
-class Save{
-    constructor(){
-        this.upward = document.getElementById("upward");
-        this.downward = document.getElementById("downward");
-        this.forward = document.getElementById("forward");
-        this.backward = document.getElementById("backward");
-        this.right = document.getElementById("right");
-        this.left = document.getElementById("left");
-        this.config = document.getElementById("config");
-    }
-    check(){
-        if (this.config.value > 10){
-            this.config.value = 10;
-        }
-    }
-    saveValues() {
-        this.check();
-        var request = new XMLHttpRequest();
-        request.open("GET", `http://127.0.0.1:5000/save?config=${this.config.value}&u=${this.upward.value}&d=${this.downward.value}&f=${this.forward.value}&b=${this.backward.value}&r=${this.right.value}&l=${this.left.value}`);
-        request.send();
-        request.addEventListener("load", function(){
-            alert(request.response)
-        })
-        console.log("Saved");
-    }
+var upward = document.getElementById("upward");
+var downward = document.getElementById("downward");
+var forward = document.getElementById("forward");
+var backward = document.getElementById("backward");
+var right = document.getElementById("right");
+var left = document.getElementById("left");
+var config = document.getElementById("config");
+var input = document.getElementById("input");
+
+function saveValues() {
+    var request = new XMLHttpRequest();
+    request.open("GET", `http://127.0.0.1:5000/save?config=${config.value}&u=${upward.value}&d=${downward.value}&f=${forward.value}&b=${backward.value}&r=${right.value}&l=${left.value}`);
+    request.send();
+    request.addEventListener("load", function(){
+        alert(request.response)
+    })
+    console.log("Saved");
 }
 
-class Load{
-    constructor(){
-        this.upward = document.getElementById("upward");
-        this.downward = document.getElementById("downward");
-        this.forward = document.getElementById("forward");
-        this.backward = document.getElementById("backward");
-        this.right = document.getElementById("right");
-        this.left = document.getElementById("left");
-        this.config = document.getElementById("config");
-    }
-    check(){
-        if (this.config.value > 10){
-            this.config.value = 10;
+function loadValues (){
+    var request = new XMLHttpRequest();
+    request.open("GET", `http://127.0.0.1:5000/load?config=${config.value}`);
+    request.send();
+    request.addEventListener("load", function(){
+        if (request.status != 200){
+            alert("An Error Has Occured!");
+            return;
         }
-    }
-    loadValues (){
-        this.check();
-        var request = new XMLHttpRequest();
-        request.open("GET", `http://127.0.0.1:5000/load?config=${this.config.value}`);
-        request.send();
-        request.addEventListener("load", function(){
-            if (request.status != 200){
-                alert("An Error Has Occured!");
-                return;
-            }
-            const response = JSON.parse(request.response)
-            self.upward.value = response["up"];
-            self.downward.value = response["down"];
-            self.forward.value = response["forward"];
-            self.backward.value = response["backward"];
-            self.right.value = response["right"];
-            self.left.value = response["left"];
-        })
-    }
+        const response = JSON.parse(request.response);
+        console.log(response);
+        upward.value = response["up"];
+        downward.value = response["down"];
+        forward.value = response["forward"];
+        backward.value = response["backward"];
+        right.value = response["right"];
+        left.value = response["left"];
+    })
+}
+
+function getValues(){
+    var config = document.getElementById("config");
+    var input = document.getElementById("input");
+    var request = new XMLHttpRequest();
+    request.open("GET", `http://127.0.0.1:5000/load?config=${config.value}`);
+    console.log("Pressed");
+    request.send();
+    request.addEventListener("load", function(){
+        console.log("IN");
+        if (request.status != 200){
+            alert("An Error Has Occured!");
+            return;
+        }
+        console.log(request.response);
+        const response = JSON.parse(request.response);
+        input.value = `{"up":"${response["up"]}","down":"${response["down"]}","forward":"${response["forward"]}","backward":"${response["backward"]}","right":"${response["right"]}","left":"${response["left"]}"}`;     
+    })
 }
--- Scripts/keyborad.js
+++ Scripts/keyborad.js
@@ -1,6 +1,7 @@
-// document.addEventListener("keydown" , (event) =>{
-//     console.log(event.key);
-// })
+var response = document.getElementById("input");
+// response = JSON.parse(response.value);
+
+
 
 class keyboard_controller{
     // constructor(){
@@ -9,7 +10,6 @@ class keyboard_controller{
 
     move_forward(){
         console.log("moving forward");
-
     }
 
     move_backward(){
@@ -37,28 +37,58 @@ class keyboard_controller{
 
 
 }
-
 let keyboard = new keyboard_controller();
 
 document.addEventListener("keydown" , (event) =>{
-    if (event.key == "w"){
-        keyboard.move_forward();
-    }
-    if (event.key == "s"){
-        keyboard.move_backward();
-    }
-    if (event.key == "a"){
-        keyboard.move_left();
-    }
-    if (event.key == "d"){
-        keyboard.move_right();
-    }
-    if (event.key == "c"){
+    // console.log(event.key);
+    // console.log(response.value);
+    // var newRes = JSON.parse(response.value);
+    var newRes = JSON.parse(response.value.replaceAll("'",'"'));
+    // console.log(newRes["up"]);
+    if (event.key == newRes["up"]){
+        console.log("Success");
         keyboard.move_up();
     }
-    if (event.key == "v"){
+    if (event.key == newRes["down"]){
         keyboard.move_down();
     }
+    if (event.key == newRes["forward"]){
+        keyboard.move_forward();
+    }
+    if (event.key == newRes["backward"]){
+        keyboard.move_backward();
+    }
+    if (event.key == newRes["right"]){
+        keyboard.move_right();
+    }
+    if (event.key == newRes["left"]){
+        keyboard.move_left();
+    }
 })
+// var response = document.getElementById("input");
+// response = JSON.parse(response.value);
+
+// document.addEventListener("keydown" , (event) =>{
+//     console.log(response);
+//     console.log(event.key);
+//     if (event.key == response["forward"]){
+//         keyboard.move_forward();
+//     }
+    // if (event.key == response["backward"]){
+    //     keyboard.move_backward();
+    // }
+    // if (event.key == "a"){
+    //     keyboard.move_left();
+    // }
+    // if (event.key == "d"){
+    //     keyboard.move_right();
+    // }
+    // if (event.key == "c"){
+    //     keyboard.move_up();
+    // }
+    // if (event.key == "v"){
+    //     keyboard.move_down();
+    // }
+// })
 
 
